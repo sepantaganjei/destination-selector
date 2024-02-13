bfa4af76c2f4ee0521999983fed66460f9e7f683
Lars Dahl <larsbda@stud.ntnu.no>
2024-02-13 12:35:40 +0100

    Added functionality for seeing destination from category page. Changes to GUI.

--- reisemalsvelger/app/all/page.tsx
+++ reisemalsvelger/app/all/page.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import styles from "./page.module.css";
-
-import Filtrer  from './components/filter'; // Import the 'Alldestination' component as a named export
+import Filtrer  from './_components/filter';
 
 export default function Home() {
         return (
             <main className={styles.main}>
                 <Filtrer/>
             </main>
-        );
+        )
     }
