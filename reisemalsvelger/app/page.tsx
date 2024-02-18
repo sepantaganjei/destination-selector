d62c7d68c7386a71fac30fc6b2e62d876d690004
Synne Raad <synnera@live.com>
2024-02-18 20:51:54 +0100

    add see all destinations button in user interface with link to different page

--- reisemalsvelger/app/page.tsx
+++ reisemalsvelger/app/page.tsx
@@ -2,11 +2,13 @@ import Image from "next/image";
 import styles from "./page.module.css";
 import HomeBanner from "@/components/HomeBanner";
 import Category from "@/components/Category";
+import SearchNavbar from "@/components/SearchNavbar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <HomeBanner />
+      <SearchNavbar />
       <Category />
     </main>
   );
