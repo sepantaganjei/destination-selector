207819e52ba912655580878fb76397323059ad4a
haukkagu <falk.bruskeland@gmail.com>
2024-02-19 08:48:34 +0100

    Lint and format navbar

--- reisemalsvelger/components/Navbar.tsx
+++ reisemalsvelger/components/Navbar.tsx
@@ -1,14 +1,16 @@
 import styles from "./navbar.module.css";
 import { FaUserAlt } from "react-icons/fa";
+import Image from "next/image";
 
 const Navbar = () => {
   return (
     <nav className={styles["navbar-container"]}>
       <div className={styles["logo-container"]}>
-        <img
+        <Image
           title="Image"
+          alt="logo"
           src="https://static.vecteezy.com/system/resources/thumbnails/024/553/534/small/lion-head-logo-mascot-wildlife-animal-illustration-generative-ai-png.png"
-        ></img>
+        />
         <div className={styles["title-container"]}>
           <h1>Ferdfinner</h1>
         </div>
