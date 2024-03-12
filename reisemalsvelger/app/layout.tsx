a41c8a18519d2d7a5e5e8d13612715b565076cc3
Sepanta Ganjei <sepantag@stud.ntnu.no>
2024-03-12 10:04:08 +0100

    Add Suspense component to RootLayout

--- reisemalsvelger/app/layout.tsx
+++ reisemalsvelger/app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import { AuthProvider } from "@/context/authContext";
 import Navbar from "@/components/Navbar";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,8 +21,10 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthProvider>
+          <Suspense>
           <Navbar />
           <main className="main">{children}</main>
+          </Suspense>
         </AuthProvider>
       </body>
     </html>
