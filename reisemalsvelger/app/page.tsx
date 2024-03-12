e7352a1e926a8a246f2d742a1b1ac9efe5b0e1ef
Lars Dahl <larsbda@stud.ntnu.no>
2024-03-12 08:32:03 +0100

    format

--- reisemalsvelger/app/page.tsx
+++ reisemalsvelger/app/page.tsx
@@ -39,7 +39,7 @@ export default function Home() {
     console.log("Henter preferanser");
     let existingPreference = await getData<Preference>("userPreference");
     let yourPreference = existingPreference.find(
-      (preference) => preference.uid === (user?.email || ""),
+      (preference) => preference.uid === (user?.email || "")
     );
     setPreferences(yourPreference?.tag.map((tag) => tag.toLowerCase()) || []);
   };
@@ -58,7 +58,7 @@ export default function Home() {
 
   const filteredDestinations = travelDestinations.filter((destination) => {
     const destinationReviews = reviews.filter(
-      (review) => review.destinationId === destination.id,
+      (review) => review.destinationId === destination.id
     );
     const averageRating =
       destinationReviews.length > 0
@@ -87,7 +87,7 @@ export default function Home() {
           key={tag}
           name={tag[0].toUpperCase() + tag.slice(1).toLowerCase()}
           travelDestinations={travelDestinations.filter((travelDestination) =>
-            travelDestination.tags.includes(tag),
+            travelDestination.tags.includes(tag)
           )}
         />
       ))}
